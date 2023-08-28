2fc5b752039a8fe3d4c2f50d3d5ad329ce361156
Tal Kehila <129615478+TalKehila@users.noreply.github.com>
2023-08-29 00:33:35 +0300

    Add files via upload

---
+++ bool check/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = false;
+            List<int> list = new List<int>() { 1, 3, 2, 4, 5 };
+            int n=0;
+            foreach (int i in list)
+            {
+                if (i == 3)
+                {
+                 flag = true;
+                 n = i;
+                }
+
+            }
+            if (flag) 
+            { 
+            
+                Console.WriteLine($"{flag} was found {n}");
+            }
+
+        } 
+    }
+}
