55515e4d53e12b4ca085f18af6ba55a6ea469747
TalKehila <129615478+TalKehila@users.noreply.github.com>
2023-08-30 11:46:23 +0300

    upload data type file

---
+++ DataType/DataType/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Data Types
+
+            //Console.WriteLine(typeof(string));
+            //Console.WriteLine(typeof(int));
+            //Console.WriteLine(typeof(float));
+            //Console.WriteLine(typeof(double));
+            //Console.WriteLine(typeof(decimal));
+
+
+            // incrament oparator
+
+            // int i = 0;
+            //i = 5;
+            //Console.WriteLine(i++); // print 5 and then change it to 6 
+            //Console.WriteLine(++i); // raise it to 6 and then print 6 
+
+            //float pint 
+
+            /*Console.WriteLine((int)5.5f);*///we get 5 we lose the 0.5 its always do the same int 5 
+
+
+            //Console.WriteLine(Math.Ceiling(4.3m));// round up 
+            //Console.WriteLine(Math.Round(4.6m));// nearest whole number output 5 
+            //Console.WriteLine(Math.Truncate(4.3f));// leaving the decimal part 
+
+           
+
+
+
+        }
+
+
+    }
+}
