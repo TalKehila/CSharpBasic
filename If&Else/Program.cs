c895c91bbb819ae7a3c2e31e018d82753a06637b
TalKehila <129615478+TalKehila@users.noreply.github.com>
2023-08-30 12:19:18 +0300

    HomeWork If and Else

---
+++ If&Else/Program.cs
@@ -0,0 +1,53 @@
+﻿
+Console.WriteLine("enter number");
+
+int x = int.Parse(Console.ReadLine());
+
+Console.WriteLine("within 0...100 ");
+if (x > 0 && x <= 100)
+{
+    Console.WriteLine("True");
+
+
+}
+else
+{
+    Console.WriteLine("False");
+}
+
+if (x >= 0 && x <= 200 && x % 2 == 0)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
+
+if (x <= 0 || x >= 100)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
+
+int y;
+Console.WriteLine("enter number");
+
+y = int.Parse(Console.ReadLine());
+
+Console.WriteLine("within 0...100 ");
+
+Console.WriteLine(y > 0 && y <= 100);
+
+
+Console.WriteLine("within  0...200  and even ");
+
+Console.WriteLine(y >= 0 && y <= 200 && y % 2 == 0);
+
+Console.WriteLine("out side 0...100 ");
+
+Console.WriteLine(y < 0 || y > 100);
+
